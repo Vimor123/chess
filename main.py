ebcb279b1b6791e1850fcbc7be16eb5763146e32
Vimor123 <vmladar1@gmail.com>
2023-05-17 14:47:28 +0200

    Started

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter
+
+root = tkinter.Tk()
+root.title("Chess")
+
+label = tkinter.Label(root, text = "Chess", font = ('Cantarell', 18))
+label.pack(padx = 20, pady = 20)
+
+root.mainloop()
