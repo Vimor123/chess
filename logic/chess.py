8150ced4d5e28cbbcac2092342eb74e2b81e9709
Vimor123 <vmladar1@gmail.com>
2023-05-17 15:46:25 +0200

    Testing importing

---
+++ logic/chess.py
@@ -0,0 +1,10 @@
+import tkinter
+
+class Game:
+
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.root.title("Chess")
+        self.root.geometry("500x500")
+
+        self.root.mainloop()
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import tkinter
+import logic.chess
 
 class StartScreen:
 
     def __init__(self):
-
         self.root = tkinter.Tk()
         self.root.title("Chess")
         self.root.geometry("300x300")
 
         self.title_label = tkinter.Label(self.root, text = "Chess", font = ('Cantarell', 24))
-        self.play_btn = tkinter.Button(self.root, text = "Play", font = ('Cantarell', 18))
+        self.play_btn = tkinter.Button(self.root, text = "Play", font = ('Cantarell', 18), command = self.play)
         self.quit_btn = tkinter.Button(self.root, text = "Exit", font = ('Cantarell', 18), command = self.quit)
 
         self.title_label.pack(pady = 10)
@@ -18,8 +18,12 @@ class StartScreen:
 
         self.root.mainloop()
 
+    def play(self):
+        self.game_window = tkinter.Tk()
+        self.game_window.mainloop()
+
     def quit(self):
-        exit(0)
+        self.root.destroy()
 
 
 StartScreen()
