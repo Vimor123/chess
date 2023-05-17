6b00b0c1974e3d743d550e4438f2af8661c5c50f
Vimor123 <vmladar1@gmail.com>
2023-05-17 15:02:48 +0200

    Playing with tkinter

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("Chess")
+root.geometry("800x800")
 
-label = tkinter.Label(root, text = "Chess", font = ('Cantarell', 18))
-label.pack(padx = 20, pady = 20)
+title_label = tkinter.Label(root, text = "Chess", font = ('Cantarell', 18))
+play_btn = tkinter.Button(root, text = "Play", font = ('Cantarell', 18))
+exit_btn = tkinter.Button(root, text = "Exit", font = ('Cantarell', 18))
+
+title_label.grid(row = 0, column = 0, sticky = tkinter.W + tkinter.E)
+play_btn.grid(row = 1, column = 0, sticky = tkinter.W + tkinter.E)
+exit_btn.grid(row = 2, column = 0, sticky = tkinter.W + tkinter.E)
 
 root.mainloop()
